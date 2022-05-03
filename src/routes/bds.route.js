e8dd9d36642c4078663fee42a1e5578262b45220
Gill <gill.eeckelaert@telenet.be>
2022-05-03 12:13:54 +0200

    POI call

--- src/routes/bds.route.js
+++ src/routes/bds.route.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const BDSController = require('../controllers/BDS.controller');
 
 
-router.get('/', BDSController.get);
-router.get('/gent', BDSController.get_sparql);
-
 /**
  * @swagger
  * /poi/{country}:
@@ -21,7 +18,7 @@ router.get('/gent', BDSController.get_sparql);
  *           type: string
  *     responses:
  *       200:
- *         description: A list of POI's.
+ *         description: A list of POI's. IMPORTANT! -photo- will not be part of the JSON object if no photo is found. Also, only if typePOI = -MUSEA-, will there be a property called -objects- containing the art objects.
  *         content:
  *           application/json:
  *             schema:
@@ -40,6 +37,10 @@ router.get('/gent', BDSController.get_sparql);
  *                         type: array
  *                         description: Types of the POI.
  *                         example: ["restaurant", "bar", "store"]
+ *                       photo:
+ *                         type: string
+ *                         description: URL of a picture of the POI.
+ *                         example: https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=Aap_uECx7h3tskxFTbgQqs2C8eFzQSy26unq5TLiuWITSzkv14bD-dmNkQNCsrC6yeagcqNiRhRdvHyXP2vjCgoNyqjdgyMJSwO4h6CLRdZNOKOF-E0GlDvJ7l0G52C2Nan0vbAa7LF7nj7Pvej-vg-XRajUk2DHG77Jo9__Riypt_kAJYCa&key=AIzaSyAZjaVJSVc5Kho17T5Eq6q3N6pkFs_zbpg
  *                       icon:
  *                         type: string
  *                         description: URL of the icon of POI.
@@ -52,8 +53,32 @@ router.get('/gent', BDSController.get_sparql);
  *                         type: float
  *                         description: The Y-coördinate of the location.
  *                         example: 3.72020
+ *                       objects:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             id:
+ *                               type: string
+ *                               description: The ID of the art object.
+ *                               example: hva:2002-078-852
+ *                             type:
+ *                               type: string
+ *                               description: The type of the art object.
+ *                               example: asset
+ *                             title:
+ *                               type: string
+ *                               description: The title of the art object.
+ *                               example: Affiche voor Cirque Pauwels
+ *                             description:
+ *                               type: string
+ *                               description: The description of the art object.
+ *                               example: Affiche voor Cirque Pauwels. Donkerblauwe achtergrond, witte randen. Gekleurde tekeningen, een lachend clownshoofd en twee steigerende circuspaarden. Tekst, Cirque Pauwels. Le nouveau Cirque de France.
+ *                             image:
+ *                               type: string
+ *                               description: The URL of the image of the art object.
+ *                               example: https://api.collectie.gent/iiif/imageiiif/3/d7b1996e3e4c622c6eeda23150dc4909-2002-078-852.tif/full/%5E1000,/0/default.jpg
 */
 router.get('/poi/:country', BDSController.get_poi);
-router.get('/poi_coghent/:country', BDSController.get_poi_coghent);
 
 module.exports = router;
--- src/services/locations.js
+++ src/services/locations.js
@@ -1,12 +1,13 @@
 const https = require('https')
 
 const API_URL = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
+const API_URL_PHOTO = 'https://maps.googleapis.com/maps/api/place/photo?'
 const API_KEY = 'AIzaSyAZjaVJSVc5Kho17T5Eq6q3N6pkFs_zbpg'
 
-const get_locations = (search) => {
+const get_locations = (search, type='restaurant', limit=3) => {
     return new Promise((resolve, reject) => {
-        let search_full = `${search} restaurant in Ghent`
-        let url = `${API_URL}?query=${search_full}&key=${API_KEY}`
+        let search_full = `${search} ${type}`
+        let url = `${API_URL}?location=51.05340993563812%23.727066260452398&query=${search_full}&radius=20000&key=${API_KEY}`
         https.get(url, (response) => {
             console.log('STATUS: ' + response.statusCode);
             console.log('MESSAGE: ' + response.statusMessage);
@@ -16,7 +17,10 @@ const get_locations = (search) => {
             });
             response.on('end', () => {
                 let places = JSON.parse(body);
-                const locations = places.results;
+                let locations = places.results;
+                if (locations.length > 5) {
+                    locations = locations.slice(0,limit)
+                }
                 resolve(locations);
             });
           }).on('error', () => {
@@ -24,5 +28,6 @@ const get_locations = (search) => {
           })
     });
 };
+
 
 exports.get_locations = get_locations;
